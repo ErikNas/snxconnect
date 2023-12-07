28811980fe7a22e28414c64a85f929ec8bf3804c
Belikov Sergey <belikov.s@rt.ru>
2023-12-07 19:57:14 +0600

    Fix: skip empty lines in '.snxvpnrc'.

--- snxconnect.py
+++ snxconnect.py
@@ -459,7 +459,7 @@ def main () :
     if cfgf :
         for line in cfgf :
             line = line.strip ().decode ('utf-8')
-            if line.startswith ('#') :
+            if len(line) == 0 or line.startswith ('#') :
                 continue
             k, v = line.split (None, 1)
             if k == 'server' :
